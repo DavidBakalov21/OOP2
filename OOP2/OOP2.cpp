e563891aee6ef1d2712b178a825aa4a4a6429a6a
DavidBakalov21 <davidbakalov33@gmail.com>
2023-11-17 22:20:59 +0200

    seems like all classes have been implemented and working, need ui, complete classes interactions

--- OOP2/OOP2.cpp
+++ OOP2/OOP2.cpp
@@ -1,12 +1,20 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
 #include <random>
 #include <chrono>
+#include <fstream>
+#include <sstream>
+
+
+
 class Product {
     
 public:
-   
+    // need it to make Product polymorphic
+    virtual ~Product() {}
+    // need it to make Product polymorphic
     Product(std::string n, int priceProd, int q) :productID(GenerateId()), name(n), price(priceProd), quantityInStock(q) {}
 
 
@@ -156,12 +164,253 @@ private:
     std::string material;
 };
 
-class Inventory {
-    Inventory() {}
+class FileConfig {
+public:
+    FileConfig() {}
+
+    void ReadReturn(const std::string& name) {
+        std::ifstream file(name);
+        if (!file.is_open()) {
+            std::cout << "Failed to open the file." << std::endl;
+            return;
+        }
+
+        std::string line;
+        while (getline(file, line)) {
+            std::vector<std::string> tokens = splitStringByComma(line);
+
+            if (tokens.empty()) {
+                return;
+            }
+
+            if (tokens[0] == "Electronics") {
+                std::shared_ptr<Electronics> electronicProduct = std::make_shared<Electronics>(tokens[1], std::stoi(tokens[2]), std::stoi(tokens[3]), tokens[4], tokens[5], std::stoi(tokens[6]));
+                ConfigItems.push_back(electronicProduct);
+            }
+            else if (tokens[0] == "Books") {
+                std::shared_ptr<Books> BookProduct = std::make_shared<Books>(tokens[1], std::stoi(tokens[2]), std::stoi(tokens[3]), tokens[4], tokens[5], tokens[6]);
+                ConfigItems.push_back(BookProduct);
+            }
+            else if (tokens[0] == "Clothing") {
+                std::shared_ptr<Clothing> ClothingProduct = std::make_shared<Clothing>(tokens[1], std::stoi(tokens[2]), std::stoi(tokens[3]), tokens[4], tokens[5], tokens[6]);
+                ConfigItems.push_back(ClothingProduct);
+            }
+
+
+        }
+
+        file.close();
+    }
+
+    std::vector<std::shared_ptr<Product>> GiveInfo() {
+        return ConfigItems;
+    }
+
+private:
+    std::vector<std::string> splitStringByComma(const std::string& str) {
+        std::vector<std::string> result;
+        std::istringstream ss(str);
+        std::string token;
+
+        while (std::getline(ss, token, ',')) {
+            result.push_back(token);
+        }
+
+        return result;
+    }
+    std::vector<std::shared_ptr<Product>> ConfigItems;
+
+
 };
+class Inventory {
+public:
+    Inventory() {}
+    void AddItem(const std::shared_ptr<Electronics>& electronicProduct) {
+        products.push_back(electronicProduct);
+    }
+    void AddProduct(const std::shared_ptr<Books>& bookProduct) {
+        products.push_back(bookProduct);
+    }
+
+    void AddProduct(const std::shared_ptr<Clothing>& clothingProduct) {
+        products.push_back(clothingProduct);
+    }
+    void UpdateQuantuty(const std::string& productName, const int newQuantity) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products[i]->getName() == productName) {
+                products[i]->Setquantity(newQuantity);
+                std::cout << "quantity set" << std::endl;
+                break;
+            }
+        }
+    }
+    void AlertQuantuty() {
+        LowStock.clear();
+        for (int i = 0; i < products.size(); i++) {
+            if (products[i]->getQuantityInStock() < 10) {
+                std::cout << products[i]->getName() << " low amount" << std::endl;
+                LowStock.push_back(products[i]);
+            }
+
+        }
+    }
+    int ShowQuantity(const std::string& productName) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products[i]->getName() == productName) {
+
+                std::cout << products[i]->getQuantityInStock() << std::endl;
+                return products[i]->getQuantityInStock();
+            }
+        }
+    }
+
+
+
+private:
+    std::vector<std::shared_ptr<Product>> products;
+
+    std::vector<std::shared_ptr<Product>> LowStock;
+};
+class Order {
+public:
+    Order(std::string oId, std::string cust, std::vector<std::shared_ptr<Product>> prod, std::string s) : orderID(oId), customer(cust), products(prod),  orderStatus(s){}
+    void addProduct(const std::shared_ptr<Product>& newProduct) {
+        products.push_back(newProduct);
+    }
+
+    void setTotalCost() {
+        
+        for (int i = 0; i < products.size(); i++) {
+            totalCost+= products[i]->getPrice();
+        }
+    }
+    void changeStatus(std::string& newStatus) {
+        orderStatus = newStatus;
+    }
+
+    int getTotalCost() {
+        return totalCost;
+    }
+    void shipProduct(Inventory& inventory, const std::string& productName) {
+        int quantity = inventory.ShowQuantity(productName);
+        if (quantity > 0) {
+            inventory.UpdateQuantuty(productName, quantity - 1);
+            std::cout << "Product shipped: " << productName <<" for " <<customer <<std::endl;
+           
+        }
+        else {
+            std::cout << "No product: " << productName << std::endl;
+            
+        }
+    }
+private:
+    std::string orderID; 
+    std::string customer;
+    std::vector<std::shared_ptr<Product>> products;
+    int totalCost;
+    std::string orderStatus;
+
+
+};
+
+class ProductCatalog {
+public:
+    ProductCatalog(){}
+    void AddProduct(const std::shared_ptr<Electronics>& electronicProduct) {
+        products.push_back(electronicProduct);
+    }
+
+    void AddProduct(const std::shared_ptr<Books>& bookProduct) {
+        products.push_back(bookProduct);
+    }
+
+    void AddProduct(const std::shared_ptr<Clothing>& clothingProduct) {
+        products.push_back(clothingProduct);
+    }
+
+    void UpdateElectronicProduct(const std::string& productName, const std::string& newBrand, const std::string& newModel, int newPowerConsumption, const std::string& newName, int newPrice) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products[i]->getName() == productName) {
+                //converting base class to derived one
+                std::shared_ptr<Electronics> electronicPtr = std::dynamic_pointer_cast<Electronics>(products[i]);
+                    electronicPtr->setBrand(newBrand);
+                    electronicPtr->setModel(newModel);
+                    electronicPtr->setPowerConsumption(newPowerConsumption);
+                    products[i]->SetName(newName);
+                    products[i]->SetPrice(newPrice);
+            }
+        }
+    }
+
+
+    void UpdateBookProduct(const std::string& productName, const std::string& newAuthor, const std::string& newGenre, int newISBN, const std::string& newName, int newPrice) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products[i]->getName() == productName) {
+                std::shared_ptr<Books> BookPtr = std::dynamic_pointer_cast<Books>(products[i]);
+                BookPtr->setAuthor(newAuthor);
+                BookPtr->setGenre(newGenre);
+                BookPtr->setISBN(newISBN);
+                products[i]->SetName(newName);
+                products[i]->SetPrice(newPrice);
+            }
+        }
+    }
+
+    void UpdateClothongProduct(const std::string& productName, const std::string& newSize, const std::string& newColor, const std::string& newMaterial, const std::string& newName, int newPrice) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products[i]->getName() == productName) {
+                std::shared_ptr<Clothing> ClothPtr = std::dynamic_pointer_cast<Clothing>(products[i]);
+                ClothPtr->setColor(newColor);
+                ClothPtr->setSize(newSize);
+                ClothPtr->setMaterial(newMaterial);
+                products[i]->SetName(newName);
+                products[i]->SetPrice(newPrice);
+            }
+        }
+    }
+
+    void RemoveProduct(const std::string& productName) {
+    for (int i = 0; i < products.size(); i++) {
+        if (products[i]->getName() == productName) {
+            products.erase(products.begin() + i);
+            break; 
+        }
+    }
+}
+
+    void ViewList() {
+        for (int i = 0; i < products.size(); i++) {
+            std::string ProductEl=products[i]->getName();
+            std::cout << ProductEl << std::endl;
+        }
+    }
+private:
+    std::vector<std::shared_ptr<Product>> products;
+    
+};
+
+
+
 int main()
 {
-    std::cout << "ff" << std::endl;
+    ProductCatalog catalog;
+    FileConfig fileConfig;
+    fileConfig.ReadReturn("C:/Users/Давід/source/repos/OOP2/OOP2/config.txt");
+
+    std::vector<std::shared_ptr<Product>> configProducts = fileConfig.GiveInfo();
+    //The point I have three conditionals is that I have three overloaded methods "AddProduct", and each method wants it's own data type
+    for (int i = 0; i < configProducts.size(); i++) {
+        if (std::shared_ptr<Electronics> elec = std::dynamic_pointer_cast<Electronics>(configProducts[i])) {
+            catalog.AddProduct(elec);
+        }
+        else if (std::shared_ptr<Books> book = std::dynamic_pointer_cast<Books>(configProducts[i])) {
+            catalog.AddProduct(book);
+        }
+        else if (std::shared_ptr<Clothing> cloth = std::dynamic_pointer_cast<Clothing>(configProducts[i])) {
+            catalog.AddProduct(cloth);
+        }
+    }
+    catalog.ViewList();
 }
 
 
